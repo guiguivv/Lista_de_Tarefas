e13194ae2b1583ed91ab15d4010c054c9861f9b7
Guilherme Valério <105113579+guiguivv@users.noreply.github.com>
2022-08-27 10:36:52 -0300

    primeiro commit

---
+++ logic/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAMXR9QcXLDlqvzwzSBc1LsgNke-1cTVrk",
+    authDomain: "to-do-list-guigui.firebaseapp.com",
+    databaseURL: "https://to-do-list-guigui-default-rtdb.firebaseio.com",
+    projectId: "to-do-list-guigui",
+    storageBucket: "to-do-list-guigui.appspot.com",
+    messagingSenderId: "814301754787",
+    appId: "1:814301754787:web:12837d95ada6bb3135d616"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
